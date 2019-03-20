6644169742e1a4e76c10731b737924af6cb44096
Gian Banares <gianbanares@gb-mbp.attlocal.net>
2019-03-20 00:14:24 -0700

    update

--- app/public/javascript/survey.js
+++ app/public/javascript/survey.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  
   var questions = [
     "I enjoy cooking.",
     "I believe I have a fantastic singing voice.",
@@ -13,92 +12,88 @@ $(document).ready(function() {
     "I can find the positive in any situation."
   ];
 
-// scale
-var choices = [
-    '1 (Strongly Disagree)',
-    '2 (Disagree)',
-    '3 (Neutral)',
-    '4 (Agree)',
-    '5 (Strongly Agree)'
-];
+  // answer scale
+  var choices = [
+    "1 (Strongly Disagree)",
+    "2 (Disagree)",
+    "3 (Neutral)",
+    "4 (Agree)",
+    "5 (Strongly Agree)"
+  ];
 
-// set count to 0.
-var questionList = $('#questions');
-i = 0;
+  // set count to 0.
+  var questionList = $("#questions");
+  i = 0;
 
-// post each question
-questions.forEach(function (question) {
+  // post each question
+  questions.forEach(function(question) {
     i++;
     var item = $('<div class="question">');
-    var headline = $('<h4>').text('Question ' + i);
-    var questionText = $('<p>').text(question);
+    var headline = $("<h4>").text("Question " + i);
+    var questionText = $("<p>").text(question);
     var dropDown = $('<div class="form-group">');
     var select = $('<select class="form-control selector">');
-    
+
     choices.forEach(function(choice) {
-        var option = $('<option>').text(choice);
-        select.append(option);
+      var option = $("<option>").text(choice);
+      select.append(option);
     });
-    select.attr('id', 'select' + i);
-    
+    select.attr("id", "select" + i);
+
     dropDown.append(select);
     item.append(headline, questionText, dropDown);
-    var br = $('<br>');
+    var br = $("<br>");
     questionList.append(item, br);
-});
+  });
 
-
-$('#submit').on('click', function(e) {
+  $("#submit").on("click", function(e) {
     e.preventDefault();
 
-    var userName = $('#userName').val();
-    var imageLink = $('#imageLink').val();
+    var userName = $("#userName").val();
+    var imageLink = $("#imageLink").val();
 
-    if (userName.length > 0 && imageLink.length >0) {
-        var answers = [];
+    if (userName.length > 0 && imageLink.length > 0) {
+      var answers = [];
 
-        Object.keys($('.selector')).forEach(function(key) {
-            if (answers.length < questions.length) {
-                answers.push($('.selector')[key].value.charAt(0));
-            }
-        });
+      Object.keys($(".selector")).forEach(function(key) {
+        if (answers.length < questions.length) {
+          answers.push($(".selector")[key].value.charAt(0));
+        }
+      });
 
-        var surveyData = {
-            name: userName,
-            photo: imageLink,
-            answers: answers
-        };
+      var surveyData = {
+        name: userName,
+        photo: imageLink,
+        answers: answers
+      };
 
-        $.post('/api/friends', surveyData, function(data) {
-
-            if (data) {
-                $('#modalContent').empty();
-                $('#userName').val('');
-                $('#imageLink').val('');
-                data.forEach(function(profile) {
-                    var profileDiv = $('<div class="profile">');
-                    var name = profile.name;
-                    var photoURL = profile.photo;
-                    var nameHeader = $('<h3>').text(name);
-                    var photo = $('<img>').attr('src', photoURL);
-                    profileDiv.append(nameHeader, photo);
-
-                    $('#modalContent').append(profileDiv);
-                });
-                if (data.length > 1) {
-                    $('.modal-title').text('Your best matches!');
-                } else {
-                    $('.modal-title').text('Your best match!');
-                }
-
-                $('#resultModal').modal();
-            }
-        });
+      $.post("/api/friends", surveyData, function(data) {
+        if (data) {
+          $("#modalContent").empty();
+          $("#userName").val("");
+          $("#imageLink").val("");
+          data.forEach(function(profile) {
+            var profileMatch = $('<div class="profile">');
+            var name = profile.name;
+            var photoURL = profile.photo;
+            var nameHeader = $("<h3>").text(name);
+            var photo = $("<img>").attr("src", photoURL);
+            profileMatch.append(nameHeader, photo);
+            $("#modalContent").append(profileMatch);
+          });
+          if (data.length > 1) {
+            $(".modal-title").text("Your best matches!");
+          } else {
+            $(".modal-title").text("Your best match!");
+          }
+          $("#resultModal").modal();
+        }
+      });
     } else {
-        $('#errorModal').modal();
-        setTimeout(function() {
-            $('#errorModal').modal('hide');
-        }, 5000);
+      $("#errorModal").modal();
+      setTimeout(function() {
+        $("#errorModal").modal("hide");
+      }, 5000);
     }
-});
+  });
 });
